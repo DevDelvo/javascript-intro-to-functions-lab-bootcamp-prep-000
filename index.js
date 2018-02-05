a488bd21819b3faab02aa55da3d5fb1c76ec7425
Kevin Delvo <kevinbdelvo@gmail.com>
2018-02-05 21:40:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function sayHiToGrandma(string) {
   }else if (string === string.toLowerCase()) {
     return "I can't hear you!";
   } else if (string === string.toUpperCase()) {
-    return "YES INDEED";
+    return "YES INDEED!";
   }
 }
