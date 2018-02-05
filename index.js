de0540a7762d02a1f0fe9619bb2ed72656c1740d
Kevin Delvo <kevinbdelvo@gmail.com>
2018-02-05 21:40:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) {
-    return "";
+   
+  if (string === "I love you, Grandma") {
+    return "I love you too"
+  }else if (string === string.toLowerCase()) {
+    return "I can't hear you!";
   } else if (string === string.toUpperCase()) {
-    return "";
+    return "YES INDEEED";
   }
 }
